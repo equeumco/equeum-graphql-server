86334f253ac17ba6385b304d56680fe91dea8296
Giedrius Tamulaitis <gtamulaitis@equeum.com>
2019-09-30 12:28:44 +0300

    Add additional user info to EqueumContext

--- dist/types/AuthenticatedUserData.d.ts
+++ dist/types/AuthenticatedUserData.d.ts
@@ -2,7 +2,13 @@
  * Object containing data about the user making the request.
  */
 interface AuthenticatedUserData {
-    /** UUID of the user making the request. */
+    /** UUID of the user making the request */
     id: string;
+    /** Display name of the user */
+    displayName?: string;
+    /** E-mail address of the user */
+    email?: string;
+    /** Role of the user */
+    role: string;
 }
 export default AuthenticatedUserData;
--- lib/types/AuthenticatedUserData.ts
+++ lib/types/AuthenticatedUserData.ts
@@ -2,8 +2,14 @@
  * Object containing data about the user making the request.
  */
 interface AuthenticatedUserData {
-  /** UUID of the user making the request. */
+  /** UUID of the user making the request */
   id: string;
+  /** Display name of the user */
+  displayName?: string;
+  /** E-mail address of the user */
+  email?: string;
+  /** Role of the user */
+  role: string;
 }
 
 export default AuthenticatedUserData;
