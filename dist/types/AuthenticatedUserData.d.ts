ace218a068668edb074614838ee9f4b81a9db0b0
Giedrius Tamulaitis <gtamulaitis@equeum.com>
2019-10-03 15:03:55 +0300

    Add machine name to authenticated payload

--- dist/types/AuthenticatedUserData.d.ts
+++ dist/types/AuthenticatedUserData.d.ts
@@ -2,13 +2,17 @@
  * Object containing data about the user making the request.
  */
 interface AuthenticatedUserData {
-    /** UUID of the user making the request */
+    /** UUID of the user making the request. */
     id: string;
-    /** Display name of the user */
-    displayName?: string;
-    /** E-mail address of the user */
-    email?: string;
     /** Role of the user */
     role: string;
+    /** Display name of the user (only found in user tokens) */
+    displayName?: string;
+    /** E-mail address of the user (only found in user tokens) */
+    email?: string;
+    /** When user is a machine, this holds name by which machine
+     * can be identified (only found in M2M tokens)
+     */
+    machineName?: string;
 }
 export default AuthenticatedUserData;
