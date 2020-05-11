169fbd582ae447a3282c64b4376c55fbf0151905
lode rosier <loderosier1@gmail.com>
2020-05-11 20:21:50 +0200

    build fix

--- dist/EqueumGraphQLServer.js
+++ dist/EqueumGraphQLServer.js
@@ -31,8 +31,6 @@ class EqueumGraphQLServer {
                 const authHeader = req.headers.authorization || '';
                 const headerParts = authHeader.split(' ');
                 const authToken = headerParts && headerParts.length > 1 ? headerParts[1] : '';
-                console.log(req.headers['user-agent']);
-                console.log("--------------");
                 const userAgent = useragent_1.default.parse(req.headers['user-agent']).toJSON();
                 const loaderInstances = {};
                 Object.keys(loaders).map((key) => {
