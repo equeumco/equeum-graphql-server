3b50af4b31cc57211fce3ddf35d4f2cbb461ae8b
lode rosier <loderosier1@gmail.com>
2020-05-14 17:53:32 +0200

    remove console

--- dist/EqueumGraphQLServer.js
+++ dist/EqueumGraphQLServer.js
@@ -31,7 +31,6 @@ class EqueumGraphQLServer {
                 const authHeader = req.headers.authorization || '';
                 const headerParts = authHeader.split(' ');
                 const authToken = headerParts && headerParts.length > 1 ? headerParts[1] : '';
-                console.log('-----user agent data in request header-----', req.headers['user-agent']);
                 const userAgent = useragent_1.default.parse(req.headers['user-agent']).toJSON();
                 const loaderInstances = {};
                 Object.keys(loaders).map((key) => {
